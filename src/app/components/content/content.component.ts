dd103d55c28555dd8b012ff5f502e38b35f17cf5
MAOJIALI <limaojia@cn.ibm.com>
2018-04-02 20:20:59 +0800

    updates

--- src/app/components/content/content.component.ts
+++ src/app/components/content/content.component.ts
@@ -7,6 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContentComponent implements OnInit {
 
+  text1 = "Jeffrey";
+  text2 = "<h1>Gloria<h1>";
+  text3 = "欢迎光临!";
+  isRed = false;
+  fontS = false;
+
+  allStyle ={
+    color:'red',
+    'font-size':'50px'
+  };
+
   constructor() { }
 
   ngOnInit() {
